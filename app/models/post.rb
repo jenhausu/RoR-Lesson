2e8b86d1dc3a35675e4c5d8e4a7e7981bcd8525b
jian-Hau, Su <jenhausu@icloud.com>
2015-07-12 08:24:19 +0000

    Callback

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true , format: {with: /\A\d/} 
+  before_save :content_upcase
+  
+  
+  before_create do 
+    self.content = title if content.blank?
+  end 
+  
+protected
+  def content_upcase
+    content.try(:upcase!)
+  end
 end
